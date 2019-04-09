e3930eafab075e022da158d34bebc5e30904f468
Erik <hall.erik@gmail.com>
2019-04-09 17:15:16 -0600

    user can now save runs

--- runs/views.py
+++ runs/views.py
@@ -2,35 +2,32 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from .forms import CreateRunForm
-
-runs = [
-    {
-        'date': timezone.now().strftime('%m/%d/%Y'),
-        'distance': 2.5,
-        'time': '10:15'
-    },
-    {
-        'date': '03/03/2019',
-        'distance': 3.1,
-        'time': '31:15'
-    },
-    {
-        'date': '01/22/2019',
-        'distance': 4.5,
-        'time': '40:55'
-    },
-]
+from .models import Run
 
 def index(request):
     if request.method == 'POST':
-        form = CreateRunForm(request.POST)
-        if form.is_valid():
-            distance = form.cleaned_data.get('distance')
-            units = form.cleaned_data.get('units')
-            messages.success(request, 'Your run has been saved.')
-            return redirect('runs-home')
-        else: 
-            messages.error(request, 'There was a problem. Please try again.')
+        if request.user.is_authenticated:
+            form = CreateRunForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                run = Run(
+                    date=data.get('date'),
+                    distance=data.get('distance'),
+                    units=data.get('units'),
+                    hours=data.get('hours'),
+                    minutes=data.get('minutes'),
+                    seconds=data.get('seconds'),
+                    user=request.user,
+                )
+                run.save()
+                messages.success(request, 'Your run has been saved.')
+                return redirect('runs-home')
+            else: 
+                messages.error(request, 'There was a problem. Please try again.')
+        else:
+            messages.warning(request, 'You need to log in to save runs.')
+            return redirect('login')
+    runs = Run.objects.order_by('-date')
     form = CreateRunForm()
     now = timezone.now().strftime('%m/%d/%Y')
     return render(
