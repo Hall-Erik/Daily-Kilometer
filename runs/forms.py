53c5d0277e468caba2140bda9ec096086dfbc1a7
Erik <hall.erik@gmail.com>
2019-04-09 10:56:02 -0600

    form styling is now in the template where it belongs.

--- runs/forms.py
+++ runs/forms.py
@@ -10,15 +10,3 @@ class CreateRunForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(CreateRunForm, self).__init__(*args, **kwargs)
-        self.fields['distance'].widget.attrs.update(
-            {'value': '00', 'width': 500})
-        # self.fields['units'].widget.attrs.update({})
-        self.fields['date'].widget.attrs.update(
-            {'id': 'datepicker', 'width': '200px'})
-        self.fields['date'].input_formats=['%d/%m/%Y']
-        self.fields['hours'].widget.attrs.update(
-            {'value': '00', 'class': 'time-input'})
-        self.fields['minutes'].widget.attrs.update(
-            {'value': '00', 'class': 'time-input'})
-        self.fields['seconds'].widget.attrs.update(
-            {'value': '00', 'class': 'time-input'})
