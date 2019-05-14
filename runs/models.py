139c92794092de889b54224ed741e189583683e9
Erik <hall.erik@gmail.com>
2019-05-14 10:23:05 -0600

    Add Gear model

--- runs/models.py
+++ runs/models.py
@@ -4,6 +4,21 @@ from django.core.validators import MinValueValidator
 from django.utils import timezone
 
 
+class Gear(models.Model):
+    '''
+    For now, this will be used for running shoes.
+    I may expand to include other types of gear in
+    the future.
+    '''
+    name = models.TextField(max_length=30)
+    date_added = models.DateField(default=timezone.now)
+    date_retired = models.DateField(null=True, blank=True, default=None)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} owned by {self.owner.username}'
+
+
 class Run(models.Model):
     _unit_choices = (('mi', 'mi'), ('km', 'km'))
 
@@ -14,10 +29,13 @@ class Run(models.Model):
     units = models.CharField(max_length=2, choices=_unit_choices)
     duration = models.DurationField(
         default=None, blank=True, null=True)
+    gear = models.ForeignKey(
+        Gear, on_delete=models.SET_NULL,
+        null=True, blank=True, default=None)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'<Run {self.date}, {self.distance}{self.units}>'
+        return f'{self.date}, {self.distance}{self.units}'
 
     def get_duration_seconds(self):
         """
@@ -26,7 +44,3 @@ class Run(models.Model):
         return self.duration.seconds \
             + self.duration.minutes*60 \
             + self.duration.hours*3600
-
-
-# class Shoe(models.Model):
-#     name = models.TextField('Gear', max_length=20)
