0cb55b7cc209fe6ef728f0c60e9bbf0e2e3d167e
Erik <hall.erik@gmail.com>
2019-05-20 14:43:12 -0600

    add user profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import hashlib
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(
+        max_length=30, blank=True, null=True, default=None)
+
+    def gravatar_url(self):
+        md5 = hashlib.md5(self.user.email.encode())
+        digest = md5.hexdigest()
+
+        return f'http://www.gravatar.com/avatar/{digest}'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- users/tests.py
+++ users/tests.py
@@ -84,3 +84,13 @@ class RegisterFormTests(TestCase):
             'password2': 'python123',
         })
         self.assertFalse(form.is_valid())
+
+
+class ProileModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            'erik', 'hall.erik@gmail.com', '1234')
+
+    def test_gravatar_url(self):
+        expected = 'http://www.gravatar.com/avatar/6764f0006df33b933b826c736d2e4274'
+        self.assertEqual(self.user.profile.gravatar_url(), expected)
