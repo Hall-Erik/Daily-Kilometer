b198e87351a268ff2f67b8a44be129b168eafc6d
Erik <hall.erik@gmail.com>
2019-05-20 14:46:33 -0600

    pep8 fixes

--- users/tests.py
+++ users/tests.py
@@ -92,5 +92,6 @@ class ProileModelTests(TestCase):
             'erik', 'hall.erik@gmail.com', '1234')
 
     def test_gravatar_url(self):
-        expected = 'http://www.gravatar.com/avatar/6764f0006df33b933b826c736d2e4274'
+        expected = 'http://www.gravatar.com/avatar/' \
+                   '6764f0006df33b933b826c736d2e4274'
         self.assertEqual(self.user.profile.gravatar_url(), expected)
