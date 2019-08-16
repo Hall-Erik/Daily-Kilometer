f044664021e1e5d115825daa30237414802513f0
Erik <hall.erik@gmail.com>
2019-08-16 13:14:27 -0600

    header observes dummy userService data

--- ng-frontend/src/app/header/header.component.ts
+++ ng-frontend/src/app/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../services/user.service';
+import { User } from '../models/user';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.user = this.userService.user.getValue();
+    this.userService.user.subscribe(user => this.user = user);
+  }
+
+  logout() {
+    this.userService.logout();
   }
 
 }
--- ng-frontend/src/app/services/user.service.ts
+++ ng-frontend/src/app/services/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, of } from 'rxjs';
+
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +11,26 @@ export class UserService {
   constructor() { }
 
   logged_in = false;
+  _user = new BehaviorSubject<User>(null);
+
+  get user() { return this._user; }
+
+  public register(): Observable<any> {
+    return of(true);
+  }
+
+  public login(username: string, password: string): Observable<User> {
+    this._user.next(new User({username: username, email: 'test@test.com'}));
+    return this._user;
+  }
+
+  public logout(): Observable<any> {
+    this._user.next(null);
+    return of(true);
+  }
+
+  public get_user(): Observable<User> {
+    this._user.next(new User({username: 'test', email: 'test@test.com'}));
+    return this._user;
+  }
 }
