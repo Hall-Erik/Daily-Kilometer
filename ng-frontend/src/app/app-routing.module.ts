cbf32ab2bd6dbc526fcaf655b7e69c4d2268d4df
Erik <hall.erik@gmail.com>
2019-08-19 11:42:31 -0600

    add routes

--- ng-frontend/src/app/app-routing.module.ts
+++ ng-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RunCreateComponent } from './runs/run-create/run-create.component';
 import { RunListComponent } from './runs/run-list/run-list.component';
+import { RunDetailComponent } from './runs/run-detail/run-detail.component';
+import { RunEditComponent } from './runs/run-edit/run-edit.component';
 
 import { RegisterComponent } from './users/register/register.component';
 import { LoginComponent } from './users/login/login.component';
@@ -12,6 +15,9 @@ import { AccountComponent } from './users/account/account.component';
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'index'},
   {path: 'index', component: RunListComponent},
+  {path: 'runs/new', component: RunCreateComponent},
+  {path: 'runs/:id', component: RunDetailComponent},
+  {path: 'runs/:id/edit', component: RunEditComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'forgot-password', component: ForgotPasswordComponent},
