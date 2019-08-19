e6483528983d1b40d37d0225583b8c6c856ece8c
Erik <hall.erik@gmail.com>
2019-08-19 16:47:24 -0600

    run detail component

--- ng-frontend/src/app/runs/run-detail/run-detail.component.ts
+++ ng-frontend/src/app/runs/run-detail/run-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { User } from '../../models/user';
+import { Run } from 'src/app/models/run';
+
+import { UserService } from '../../services/user.service';
+import { RunService } from '../../services/run.service';
 
 @Component({
   selector: 'app-run-detail',
@@ -6,10 +13,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./run-detail.component.css']
 })
 export class RunDetailComponent implements OnInit {
+  run: Run;
+  user: User = this.userService.user.getValue();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private runService: RunService,
+              private userService: UserService) { }
 
   ngOnInit() {
+    let run_id = this.route.snapshot.paramMap.get('id');
+    this.runService.get_run(+run_id).subscribe((run) => {
+      this.run = run;
+    });
+    this.userService.user.subscribe(user => this.user = user);
   }
 
+  delete() {
+    console.log('Do a delete.');
+  }
 }
--- ng-frontend/src/app/services/run.service.ts
+++ ng-frontend/src/app/services/run.service.ts
@@ -14,4 +14,8 @@ export class RunService {
   public get_runs(): Observable<Run[]> {
     return this.http.get<Run[]>('/api/runs/');
   }
+
+  public get_run(id: number): Observable<Run> {
+    return this.http.get<Run>(`/api/runs/${id}/`);
+  }
 }
