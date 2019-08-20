8828c0750e85609b0733b87207459d29816c929a
Erik <hall.erik@gmail.com>
2019-08-20 17:39:09 -0600

    run form almost finished

--- ng-frontend/src/app/runs/run-form/run-form.component.ts
+++ ng-frontend/src/app/runs/run-form/run-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { Run } from '../../models/run';
 
 @Component({
   selector: 'app-run-form',
@@ -6,10 +9,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./run-form.component.css']
 })
 export class RunFormComponent implements OnInit {
+  @Output() runSubmit = new EventEmitter<Run>();
 
-  constructor() { }
-
-  ngOnInit() {
+  @Input() set run(initial: Run) {
+    if (initial) {
+      this.runForm.patchValue(initial);
+    }
   }
 
+  runForm = this.fb.group({
+    distance: ['', Validators.required],
+    units: ['mi', Validators.required],
+    hours: ['', [Validators.min(0)]],
+    minutes: ['', [Validators.min(0), Validators.max(59)]],
+    seconds: ['', [Validators.min(0), Validators.max(59)]],
+    date: ['', Validators.required],
+    gear: '',
+    run_type: '',
+    description: ''
+  });
+
+  get distance() { return this.runForm.get('distance'); }
+  get units() { return this.runForm.get('units'); }
+  get duration() {
+    let hours = this.runForm.get('hours');
+    let minutes = this.runForm.get('minutes');
+    let seconds = this.runForm.get('seconds');
+    if(!hours && !minutes && !seconds) {
+      return null;
+    }
+    return null;
+  }
+  get hours() { return this.runForm.get('hours'); }
+  get minutes() { return this.runForm.get('minutes'); }
+  get seconds() { return this.runForm.get('seconds'); }
+  get date() { return this.runForm.get('date'); }
+  get gear() { return this.runForm.get('gear'); }
+  get run_type() { return this.runForm.get('run_type'); }
+  get description() { return this.runForm.get('description'); }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    if (this.date.value === '') {
+      let d = new Date();
+      let date = '' + d.getDate();
+      let month = '' + (d.getMonth() + 1);
+      let year = d.getFullYear();
+
+      if (month.length < 2) month = '0' + month;
+      if (date.length < 2) date = '0' + date;
+
+      this.date.patchValue([year, month, date].join('-'));
+    }
+  }
+
+  onSubmit() {
+    this.runSubmit.emit(new Run(this.runForm.value));
+  }
 }
--- ng-frontend/src/app/runs/run-list/run-list.component.ts
+++ ng-frontend/src/app/runs/run-list/run-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RunService } from '../../services/run.service';
+import { UserService } from '../../services/user.service';
 import { Run } from '../../models/run';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-run-list',
@@ -10,13 +12,19 @@ import { Run } from '../../models/run';
 })
 export class RunListComponent implements OnInit {
   runs: Run[];
+  user: User = this.userService.user.getValue();
 
-  constructor(private runService: RunService) { }
+  constructor(private runService: RunService,
+              private userService: UserService) { }
 
   ngOnInit() {
-    this.runService.get_runs().subscribe((runs) => {
-      this.runs = runs;
-    });
+    this.runService.get_runs().subscribe(runs => this.runs = runs);
+    this.userService.user.subscribe(user => this.user = user);
   }
 
+  submit(run: Run) {
+    console.log('submit');
+    console.log(run);
+    // TODO: send to server
+  }
 }
