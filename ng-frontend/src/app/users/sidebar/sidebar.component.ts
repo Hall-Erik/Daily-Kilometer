f06a11df6b4bb35b2aa869e7375e0050635fdb40
Erik <hall.erik@gmail.com>
2019-08-23 14:22:16 -0600

    add sidebar

---
+++ ng-frontend/src/app/users/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { UserService } from '../../services/user.service';
+
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  user: User = this.userService.user.getValue();
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.user.subscribe(user => this.user = user);
+  }
+
+}
