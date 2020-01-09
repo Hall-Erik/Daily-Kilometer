689b1d7e9279aa5093e7bc7dac5b1fc2ef5ebd66
Erik <hall.erik@gmail.com>
2020-01-09 13:50:02 -0700

    replace header

--- ng-frontend/src/app/material/material.module.ts
+++ ng-frontend/src/app/material/material.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatMenuModule } from '@angular/material/menu'; 
+import { MatDividerModule } from '@angular/material/divider'; 
+import { MatIconModule } from '@angular/material/icon'; 
 
 const material = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatDividerModule,
+  MatIconModule
 ];
 
 @NgModule({
