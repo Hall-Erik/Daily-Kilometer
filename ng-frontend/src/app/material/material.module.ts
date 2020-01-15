5526b3f4d238b2b936e6115aa0f12b2d505d507a
Erik <hall.erik@gmail.com>
2020-01-15 11:31:26 -0700

    change sidebar to material card

--- ng-frontend/src/app/material/material.module.ts
+++ ng-frontend/src/app/material/material.module.ts
@@ -5,13 +5,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu'; 
 import { MatDividerModule } from '@angular/material/divider'; 
 import { MatIconModule } from '@angular/material/icon'; 
+import { MatCardModule } from '@angular/material/card'; 
+import { MatListModule } from '@angular/material/list';
 
 const material = [
   MatToolbarModule,
   MatButtonModule,
   MatMenuModule,
   MatDividerModule,
-  MatIconModule
+  MatIconModule,
+  MatCardModule,
+  MatListModule
 ];
 
 @NgModule({
